d85da0f930bb699b991799d930de48be52db4405
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-09-29 22:21:28 +0530

    Added Login page

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,55 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const reqBody = await req.json();
+    const { email, password } = reqBody;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+
+    // Check if password is correct
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid Password" }, { status: 400 });
+    }
+
+    // Create token data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    // Create token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+    
+    // Storing token in cookie
+    const response = NextResponse.json({
+      message: "Login Successfully",
+      success: true,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
---
+++ src/app/login/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+
+      // const response = axios.post("/api/users/login", user);
+      // toast.promise(response, {
+      //   loading: "Loading...",
+      //   success: (data) => `${data}`,
+      //   error: (data) => `${data}`,
+      // });
+
+      const response = await axios.post("/api/users/login", user)
+
+      console.log(response.data);
+
+      router.push("/profile");
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>{loading ? "Loading..." : "Login"}</h1>
+        <hr />
+        {/* Email */}
+        <label htmlFor="email">Email</label>
+        <input
+          className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+          type="email"
+          value={user.email}
+          id="email"
+          placeholder="Email"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setUser({ ...user, email: e.target.value });
+          }}
+        />
+        {/* Password */}
+        <label htmlFor="password">Password</label>
+        <input
+          className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+          type="password"
+          value={user.password}
+          id="password"
+          placeholder="Password"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setUser({ ...user, password: e.target.value });
+          }}
+        />
+        <button
+          onClick={onLogin}
+          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        >
+          Signup here
+        </button>
+        <Link href="/signup">Not a user? Signup here</Link>
+      </div>
+    </>
+  );
+}
